688fb9be00aea5290f60872cb8dbddf87f0b582d
mjlynch123 <mjlynch35@yahoo.com>
2021-09-24 18:32:05 -0500

    Added windows version

---
+++ eat.py
@@ -0,0 +1,57 @@
+import warnings
+from random import choice
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import links
+ 
+options = Options()
+
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver = webdriver.Chrome(executable_path="C:\\Users\\l3gon\\Desktop\\python\\chromedriver.exe",chrome_options=options)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+restaurants = ['pancheros', 'taco bell', 'dominos', 'hardees', 'wendys', 
+               'chick fil a', 'subway', 'panda express', 'arbys', 'pizza hut',
+               'panera', 'mcdonalds', 'burger king', 'happy wok']
+
+def restChoice(restaraunt):
+    if where_to_eat == 'pancheros':
+        driver.get(links.panchysLink)
+    elif where_to_eat == 'taco bell':
+        driver.get(links.tacoLink)
+    elif where_to_eat == 'dominos':
+        driver.get(links.dominosLink)
+    elif where_to_eat == 'hardees':
+        driver.get(links.hardeesLink)
+    elif where_to_eat == 'wendys':
+        driver.get(links.wendysLink)
+    elif where_to_eat == 'chick fil a':
+        driver.get(links.chickLink)
+    elif where_to_eat == 'subway':
+        driver.get(links.subwayLink)
+    elif where_to_eat == 'panda express':
+        driver.get(links.pandaLink)
+    elif where_to_eat == 'arbys':
+        driver.get(links.arbyLink)
+    elif where_to_eat == 'pizza hut':
+        driver.get(links.pizzahutLink)
+    elif where_to_eat == 'panera':
+        driver.get(links.paneraLink)
+    elif where_to_eat == 'mcdonalds':
+        driver.get(links.mcdicks)
+    elif where_to_eat == 'burger king':
+        driver.get(links.bk)
+    elif where_to_eat == 'happy wok':
+        driver.get(links.happywok)
+    else:
+        print('No restaraunt selected')
+        driver.close()
+
+while True:
+    where_to_eat = choice(restaurants)
+    print(where_to_eat.title())
+    restChoice(where_to_eat)
+    break
+
+
